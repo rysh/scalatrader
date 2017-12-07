5d30e70b3b28519bd849096249e797edd8dbc5df
r.ishibashi <rysh.cact@gmail.com>
2017-12-07 09:10:48 +0900

    refactor: Easier adjustment of loss cut standards / remove log

--- scalatrader/app/domain/strategy/turtle/TurtleStrategy.scala
+++ scalatrader/app/domain/strategy/turtle/TurtleStrategy.scala
@@ -21,17 +21,16 @@ class TurtleStrategy(user: User) extends Strategy {
   var orderSize = 0.2 * leverage
   var position: Option[Ordering] = None
   def entry(o: Ordering): Option[Ordering] = {
-    println("entry")
     position = Some(o)
     updateSizeUnit
     position
   }
   def close = {
-    println("close")
     position = None
     losLimit = None
   }
 
+  val stopRange:Option[Double] = Some(3000)
   var losLimit:Option[Double] = None
   override def judgeByTicker(ticker: Ticker): Option[Ordering] = {
     val ltp = ticker.ltp
@@ -43,11 +42,9 @@ class TurtleStrategy(user: User) extends Strategy {
       val box20 = core.box20sec.get
       if (position.isEmpty) {
         if (box20.high < ltp) {
-          println("box20.high < ltp")
           losLimit = None
           entry(Ordering(Side.Buy, orderSize))
         } else if (ltp < box20.low) {
-          println(s"ltp($ltp) < box20.low(${box20.low})")
           losLimit = None
           entry(Ordering(Side.Sell, orderSize))
         } else {
@@ -55,32 +52,26 @@ class TurtleStrategy(user: User) extends Strategy {
         }
       } else if (position.get.side == Side.Sell) {
         if (losLimit.exists(_ < ltp)) {
-          println(s"limit(${losLimit.get}) < ltp($ltp)")
           close
           Some(Ordering(Side.Buy, position.map(_.size).getOrElse(orderSize)))
         } else if (box10.high < ltp) {
-          println(s"box10.high(${box10.high}) < ltp($ltp)")
           close
           Some(Ordering(Side.Buy, position.map(_.size).getOrElse(orderSize)))
         } else if (ltp < box20.low) {
-          //println(s"ltp($ltp) < box20.low(${box20.low}) not order because already have position")
-          losLimit = Some(ltp + 3000)
+          losLimit = stopRange.map(ltp + _)
           None
         } else {
           None
         }
       } else { // BUY
         if (losLimit.exists(ltp < _)) {
-          println(s"ltp($ltp) < limit(${losLimit.get})")
           close
           Some(Ordering(Side.Sell, position.map(_.size).getOrElse(orderSize)))
         } else if (ltp < box10.low) {
-          println(s"ltp($ltp) < box10.low(${box10.low})")
           close
           Some(Ordering(Side.Sell, position.map(_.size).getOrElse(orderSize)))
         } else if (box20.high < ltp) {
-          //println(s"box20.high(${box20.high}) < ltp($ltp) not order because already have position")
-          losLimit = Some(ltp - 3000)
+          losLimit = stopRange.map(ltp - _)
           None
         } else {
           None
