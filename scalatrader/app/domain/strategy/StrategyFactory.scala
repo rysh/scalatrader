b86951446e018b077d27753bdb4a891bca504f2a
r.ishibashi <rysh.cact@gmail.com>
2018-01-09 12:26:11 +0900

    add new strategy

--- scalatrader/app/domain/strategy/StrategyFactory.scala
+++ scalatrader/app/domain/strategy/StrategyFactory.scala
@@ -1,6 +1,7 @@
 package domain.strategy
 
-import domain.strategy.box.{BoxReverseLimitStrategy, MixedBoxesStrategy}
+import domain.strategy.StrategyFactory.BoxTrendStrategy
+import domain.strategy.box.{BoxReverseLimitStrategy, MixedBoxesStrategy, BoxTrendStrategy}
 import domain.strategy.momentum.MomentumReverseStrategy
 import domain.strategy.turtle.TurtleStrategy
 import repository.model.scalatrader.User
@@ -9,9 +10,11 @@ object StrategyFactory {
   val MomentumReverse = "MomentumReverse"
   val BoxReverseLimit = "BoxReverseLimit"
   val MixedBoxesStrategy = "MixedBoxesStrategy"
+  val BoxTrendStrategy = "BoxTrendStrategy"
   val options = Map(MomentumReverse -> MomentumReverse,
     BoxReverseLimit -> BoxReverseLimit,
     MixedBoxesStrategy -> MixedBoxesStrategy,
+    BoxTrendStrategy -> BoxTrendStrategy,
     "Turtle" -> "Turtle")
 
   def create(state: StrategyState, user:User): Strategy = {
@@ -20,6 +23,7 @@ object StrategyFactory {
       case "Turtle" => new TurtleStrategy(state, user)
       case "BoxReverseLimit" => new BoxReverseLimitStrategy(state, user)
       case "MixedBoxesStrategy" => new MixedBoxesStrategy(state, user)
+      case "BoxTrendStrategy" => new BoxTrendStrategy(state, user)
       case _ => throw new Exception()
     }
   }
---
+++ scalatrader/app/domain/strategy/box/BoxTrendStrategy.scala
@@ -0,0 +1,73 @@
+package domain.strategy.box
+
+import java.time.ZonedDateTime
+
+import domain.Side._
+import domain.models.{Ticker, Ordering}
+import domain.strategy.core.Box
+import domain.strategy.{StrategyState, Strategies, Strategy}
+import domain.time.DateUtil
+import repository.model.scalatrader.User
+
+
+class BoxTrendStrategy(st: StrategyState, user: User) extends Strategy(st, user) {
+
+  var stop: Option[Double] = None
+  var entryTime: Option[ZonedDateTime] = None
+
+  override def judgeByTicker(ticker: Ticker): Option[Ordering] = {
+    val now = DateUtil.now()
+    val momentum = Strategies.coreData.momentum5min.values.takeRight(1).values.headOption
+
+    def mid(box1h: Box) = {
+      (box1h.high + box1h.low) / 2
+    }
+
+    val result = if (!isAvailable || Strategies.coreData.box1h.isEmpty || momentum.isEmpty) {
+      None
+    } else {
+      val box10min = Strategies.coreData.box10min.get
+      val box20min = Strategies.coreData.box20min.get
+      val box1h = Strategies.coreData.box1h.get
+      if (state.order.isEmpty) {
+
+        if (box10min.isUp && box20min.isUp && box1h.isUp && mid(box1h) < ticker.ltp) {
+          stop = Some(box1h.low)
+          entry(Buy)
+        } else if (box10min.isDown && box20min.isDown && box1h.isDown && mid(box1h) > ticker.ltp) {
+            stop = Some(box1h.high)
+            entry(Sell)
+        } else {
+          None
+        }
+      } else  {
+        if (state.order.get.side == Buy) {
+
+          def isUpdatingHigh = box1h.isUpdatingHigh(now, 60 * 20)
+          if (!(box10min.isUp && box20min.isUp && box1h.isUp) || mid(box1h) > ticker.ltp) {
+            close()
+          } else if (stop.exists(_ > ticker.ltp)) {
+            close()
+          } else {
+            None
+          }
+        } else { // Sell
+          def isUpdatingLow = box1h.isUpdatingLow(now, 60 * 20)
+          if (!(box10min.isDown && box20min.isDown && box1h.isDown) || mid(box1h) < ticker.ltp) {
+            close()
+          } else if (stop.exists(_ < ticker.ltp)) {
+            close()
+          } else {
+            None
+          }
+        }
+      }
+    }
+    result
+  }
+
+  override def entry(size: String): Option[Ordering] = {
+    entryTime = Some(DateUtil.now())
+    super.entry(size)
+  }
+}
