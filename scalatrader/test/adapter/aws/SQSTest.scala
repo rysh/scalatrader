d24dcce54ab4194a581030466a66629ae6f2d959
r.ishibashi <rysh.cact@gmail.com>
2018-02-20 14:19:41 +0900

    fix tests

--- scalatrader/test/adapter/aws/SQSTest.scala
+++ scalatrader/test/adapter/aws/SQSTest.scala
@@ -8,20 +8,22 @@ import org.scalatest.FunSuite
 class SQSTest extends FunSuite {
 
   ignore("testSend") {
-    SQS.send(OrderQueueBody("hoge@scalatrader.com", "hoge1", DateUtil.now().toString))
-    SQS.send(OrderQueueBody("hoge@scalatrader.com", "hoge2", DateUtil.now().toString))
-    SQS.send(OrderQueueBody("hoge@scalatrader.com", "hoge3", DateUtil.now().toString))
+    SQS.send(OrderQueueBody("hoge@scalatrader.com", 1L, "hoge1", DateUtil.now().toString))
+    SQS.send(OrderQueueBody("hoge@scalatrader.com", 2L, "hoge2", DateUtil.now().toString))
+    SQS.send(OrderQueueBody("hoge@scalatrader.com", 3L, "hoge3", DateUtil.now().toString))
   }
 
   ignore("testList") {
     val messages = SQS.list()
     println(messages.size)
-    messages.map(message => {
-      SQS.deleteOrder(message)
+    messages
+      .map(message => {
+        SQS.deleteOrder(message)
 
-      val hoge = SQS.map(message)
-      println(ZonedDateTime.parse(hoge.timestamp))
-      hoge
-    }).foreach(println)
+        val hoge = SQS.map(message)
+        println(ZonedDateTime.parse(hoge.timestamp))
+        hoge
+      })
+      .foreach(println)
   }
 }
